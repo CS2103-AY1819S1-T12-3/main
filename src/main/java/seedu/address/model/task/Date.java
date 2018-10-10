6ae68004298a1de98311e84c6f504342ef7cf936
hotarumin <>
2018-10-10 19:05:38 +0800

    fix code to pass checkstyle

--- src/main/java/seedu/address/model/task/Date.java
+++ src/main/java/seedu/address/model/task/Date.java
@@ -88,9 +88,9 @@ public class Date {
      * @param month A month
      */
     private static boolean isLargeMonth(int month) {
-        return month == 1 || month == 3 || month == 5 ||
-                month == 7 || month == 8 ||
-                month == 10 || month == 12;
+        return month == 1 || month == 3 || month == 5
+                || month == 7 || month == 8
+                || month == 10 || month == 12;
     }
 
     @Override
